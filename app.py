ac1e95949e165a3bc60fe154aca121d753ad8746
Ramakrishna <ramkrishnac@hotmail.com>
2025-09-13 08:48:33 +0530

    Renamed app1.py to app.py for OpenShift compatibility

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, OpenShift!"
+    return "Hello this is Kavya Sree Chelluri, Nanna AND Amma!"
 
-# Optional: local testing support
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
