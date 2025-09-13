46b2caa50ddc04aa1868319856c374a87b9a15b4
Ramakrishna <ramkrishnac@hotmail.com>
2025-09-13 12:26:37 +0530

    Serve static index.html from app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 
-@app.route("/")
-def hello():
-    return "Hello this is Kavya Sree Chelluri, Nanna AND Amma!"
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
